89a2dfdb2155dea20c40ef92b9b6a0484f9f90de
manchandajayant <maanjayant@gmail.com>
2020-02-09 17:48:38 +0100

    new additions

--- src/transform.js
+++ src/transform.js
@@ -1,20 +1,23 @@
 function groupAdultsByAgeRange(arr) {
   const result = {
-    "20 and younger": [],
-    "21-30": [],
-    "31-40": [],
-    "41-50": [],
-    "51 and older": []
+    "20 and younger": [{}],
+    "21-30": [{}],
+    "31-40": [{}],
+    "41-50": [{}],
+    "51 and older": [{}]
   };
   arr.forEach(person => {
     if (person.age > 17 && person.age < 21)
-      result["20 and younger"].push(person);
-    else if (person.age > 20 && person.age < 31) result["21-30"].push(person);
-    else if (person.age > 30 && person.age < 41) result["21-30"].push(person);
-    else if (person.age > 40 && person.age < 51) result["21-30"].push(person);
-    else if (person.age > 50) result["21-30"].push(person);
+      result["20 and younger"].push({ person });
+    else if (person.age > 20 && person.age < 31)
+      result["21-30"].push({ person });
+    else if (person.age > 30 && person.age < 41)
+      result["21-30"].push({ person });
+    else if (person.age > 40 && person.age < 51)
+      result["21-30"].push({ person });
+    else if (person.age > 50) result["21-30"].push({ person });
   });
-  console.log(result);
+  return result;
 }
 
 module.exports = { groupAdultsByAgeRange };
